dbd338744ecf363e28cbf5f4752bbcffe13589a2
Shohei Okazaki <59222700+ShoheiOkazaki@users.noreply.github.com>
2021-03-17 18:25:40 +0900

    New : FFmpeg Rop

---
+++ common/soho/ffmpeg_rop.py
@@ -0,0 +1,16 @@
+import soho
+import subprocess
+
+
+get_ffmpeg     = soho.getDefaultedString("ffmpeg_path",[""])[0]
+get_inputfile  = soho.getDefaultedString("ff_inputfile_with_pad",[""])[0]
+get_inputArgs  = soho.getDefaultedString("ff_inputArgs",[""])[0]
+get_outArgs    = soho.getDefaultedString("ff_outputArgs",[""])[0]
+get_outputfile = soho.getDefaultedString("ff_outputFile",[""])[0]
+
+
+cmd1 = '{} {} -i "{}" {} -y "{}"'.format(get_ffmpeg, get_inputArgs, get_inputfile, get_outArgs, get_outputfile)
+
+print '[FFmpeg_ROP] {}'.format(cmd1)
+
+subprocess.call(cmd1, shell=True)
