dcb368005bb162901e0be13f8225b14477203eed
Shohei Okazaki <zakishoheioka@gmail.com>
2021-04-25 12:32:00 +0900

    new

---
+++ common/scripts/python/nagamochi_utils.py
@@ -0,0 +1,24 @@
+import os
+import datetime
+import sys
+
+import hou
+import nodesearch
+
+def MSG(text_name,text_body,date_enable=True,print_enbale=True,StatusMessage_enbale=False,StatusMessage_type=hou.severityType.Message):
+	
+	dt_now = datetime.datetime.now()
+	startDate= dt_now.strftime('%Y-%m-%d %H:%M:%S')
+
+	msg = '[Nagamochi][{}] {}'.format(text_name,text_body)
+
+	if date_enable:
+		msg = '{}  {}'.format(startDate,msg)
+
+	if print_enbale:
+		print(msg)
+
+	if StatusMessage_enbale:
+		hou.ui.setStatusMessage(msg,severity=StatusMessage_type)
+
+	return msg
